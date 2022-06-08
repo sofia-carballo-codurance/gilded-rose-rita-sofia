c3c2353cee0877ce0a67d231079558732197d4d9
Sofia Carballo <sofia.carballo@codurance.com>
2022-06-08 16:56:16 +0200

    refactor: still in green

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -43,9 +43,8 @@ class GildedRose {
 
                     item.sellIn = item.sellIn - 1;
 
-
                     if (item.sellIn < 0) {
-                            item.quality = 0;
+                        item.quality = 0;
                     }
                     break;
 
---
+++ Java/src/main/java/com/gildedrose/ItemRita.java
@@ -0,0 +1,10 @@
+package com.gildedrose;
+
+public class ItemRita{
+
+    IUpdateQuality qualityUpgrader;
+
+    ItemRita(IUpdateQuality qualityUpgrader) {
+        this.qualityUpgrader = qualityUpgrader;
+    }
+}
