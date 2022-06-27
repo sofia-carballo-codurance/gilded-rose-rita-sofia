dcc0189a7052f130747527ce65a795b704564958
Rita Teixeira <rita.teixeira@codurance.com>
2022-06-27 15:42:58 +0200

    refactor if conditions

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,56 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+  Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
 
-    public void updateQuality() {
-        for (Item item : items) {
-            switch (item.name) {
-                case "Aged Brie":
-                    if (item.quality < 50) {
-                        upgradeQuality(item);
-                    }
+  void updateQuality() {
+    for (Item item : items) {
+      switch (item.name) {
+        case "Aged Brie":
+          if (item.quality < 50) {
+            upgradeQuality(item);
+          }
 
-                    item.sellIn -= 1;
+          item.sellIn -= 1;
 
-                    if (item.quality < 50) {
-                        if (item.sellIn < 0) {
-                            upgradeQuality(item);
-                        }
-                    }
-                    break;
+          if (item.quality < 50 && item.sellIn < 0) {
+            upgradeQuality(item);
+          }
+          break;
 
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    if (item.quality < 50) {
-                        upgradeQuality(item);
+        case "Backstage passes to a TAFKAL80ETC concert":
+          if (item.quality < 50) {
+            upgradeQuality(item);
 
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                upgradeQuality(item);
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                upgradeQuality(item);
-                            }
-                        }
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                    break;
-
-                case "Sulfuras, Hand of Ragnaros":
-                    break;
-
-                default:
+            if (item.sellIn < 11 && item.quality < 50) {
+              upgradeQuality(item);
             }
-        }
-    }
 
-    private void upgradeQuality(Item item) {
-        item.quality += 1;
+            if (item.sellIn < 6 && item.quality < 50) {
+              upgradeQuality(item);
+            }
+          }
+
+          item.sellIn = item.sellIn - 1;
+
+          if (item.sellIn < 0) {
+            item.quality = 0;
+          }
+          break;
+
+        case "Sulfuras, Hand of Ragnaros":
+          break;
+
+        default:
+      }
     }
+  }
+
+  private void upgradeQuality(Item item) {
+    item.quality += 1;
+  }
 }
